b5766dbfa0467c40b799f91a3064be978a02bf17
David C Ellis <ducksual@gmail.com>
2023-01-10 12:22:44 +0000

    Basic register test

---
+++ tests/test_register.py
@@ -0,0 +1,44 @@
+from json_defaults import JSONRegister
+
+import json
+import dataclasses
+from pathlib import Path
+from decimal import Decimal
+
+
+def test_json_register():
+    register = JSONRegister()
+
+    @dataclasses.dataclass
+    class Demo:
+        id: int
+        name: str
+        location: Path
+        numbers: list[Decimal]
+
+        @register.register_method
+        def to_json(self):
+            return {
+                'id': self.id,
+                'name': self.name,
+                'location': self.location,
+                'numbers': self.numbers,
+            }
+
+
+    register.register(Path, str)
+
+
+    @register.register_function(Decimal)
+    def unstructure_decimal(val):
+        return {'cls': 'Decimal', 'value': str(val)}
+
+
+    numbers = [Decimal(f"{i}")/Decimal('1000') for i in range(1, 3)]
+    pth = Path("usr/bin/python")
+
+    demo = Demo(id=42, name="Demonstration Class", location=pth, numbers=numbers)
+
+    output = '{"id": 42, "name": "Demonstration Class", "location": "usr/bin/python", "numbers": [{"cls": "Decimal", "value": "0.001"}, {"cls": "Decimal", "value": "0.002"}]}'
+
+    assert json.dumps(demo, default=register.default) == output
